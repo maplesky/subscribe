6cad6aae6640a7c1dec82ab943d2c6688126f654
maple's sky <maplesky@mail.com>
2026-09-15 23:28:01 +0800

    Update ruleset.py

--- .github/workflows/ruleset.py
+++ .github/workflows/ruleset.py
@@ -6,6 +6,6 @@ rule = [
 
 for node in rule:
 	subprocess.run(
-		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", node["to"], "-O", f"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/{ node['url'] }/{ node['url'] }.list"],
+		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", node["to"], "-O", f"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/{ node['name'] }/{ node['name'] }.list"],
 		check=True,
 	)
