a7971251b83cf3cbd0f815703e2df78b08f0e940
maple's sky <maplesky@mail.com>
2026-09-15 23:33:59 +0800

    Modify ruleset.py to download rules for Loon and Clash
    Updated subprocess calls to fetch rules for both Loon and Clash formats.

--- .github/workflows/ruleset.py
+++ .github/workflows/ruleset.py
@@ -1,11 +1,16 @@
 import subprocess
 
 rule = [
-	{ "name": "Twitter", "to": "rule" }
+	"Twitter"
 ]
 
 for node in rule:
 	subprocess.run(
-		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", node["to"], "-O", f"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/{ node['name'] }/{ node['name'] }.list"],
+		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", "rule/loon", "-O", f"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/{ node }/{ node }.list"],
+		check=True,
+	)
+
+	subprocess.run(
+		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", "rule/clash", "-O", f"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/{ node }/{ node }.list"],
 		check=True,
 	)
