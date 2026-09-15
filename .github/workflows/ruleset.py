8276164c4e6238222418a7fba6c3b8499ecd8e75
maple's sky <maplesky@mail.com>
2026-09-15 23:22:07 +0800

    Update ruleset.py

--- .github/workflows/ruleset.py
+++ .github/workflows/ruleset.py
@@ -1,15 +1,11 @@
 import subprocess
-from pathlib import Path
 
 rule = [
 	{ url: "blackmatrix7/ios_rule_script/master/rule/Loon/Twitter/Twitter.list", to: "rule" }
 ]
 
-fetch = lambda url, to: subprocess.run(
-    ["curl", "-fsSL", "--retry", "3", "--create-dirs", "-o", "{ to }/", "https://raw.githubusercontent.com/{url}"],
-    check=True,
-)
-
-for node in tasks:
-    fetch( node["url"], node["to"] )
-    print(f"✓ {out}")
+for node in rule:
+	subprocess.run(
+		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", node["to"], "-O", "https://raw.githubusercontent.com/{ node["url"] }"],
+		check=True,
+	)
