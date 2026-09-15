49f323485f2716f835b9167686f5cf68857a162d
maple's sky <maplesky@mail.com>
2026-09-15 23:27:13 +0800

    Update ruleset.py

--- .github/workflows/ruleset.py
+++ .github/workflows/ruleset.py
@@ -1,11 +1,11 @@
 import subprocess
 
 rule = [
-	{ "url": "blackmatrix7/ios_rule_script/master/rule/Loon/Twitter/Twitter.list", "to": "rule" }
+	{ "name": "Twitter", "to": "rule" }
 ]
 
 for node in rule:
 	subprocess.run(
-		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", node["to"], "-O", f"https://raw.githubusercontent.com/{ node['url'] }"],
+		["curl", "-fsSL", "--retry", "3", "--create-dirs", "--output-dir", node["to"], "-O", f"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/{ node['url'] }/{ node['url'] }.list"],
 		check=True,
 	)
