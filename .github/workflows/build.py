01ef4c6329bd8b31067e77d03a09b23622aff149
maple's sky <maplesky@mail.com>
2026-09-17 21:03:40 +0800

    Update build.py

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -24,7 +24,7 @@ basic = [
 # 多线程处理
 with ThreadPoolExecutor( max_workers = 25 ) as pool:
 	results = pool.map(
-			lambda node: ( node["meta"], session.get(f"https://api.lvxiaodong.com/api/tcping?address={ node["ip"] }&port={ node["port"][0] }")
+			lambda node: ( node["meta"], session.get(f"https://v2.xxapi.cn/api/tcping?address={ node["ip"] }&port={ node["port"][0] }")
 		),
 		basic
 	)
