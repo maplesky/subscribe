8561f1158969afd58ed51c1d0792b4619b543dfd
maple's sky <maplesky@mail.com>
2026-09-17 22:58:57 +0800

    Update build.py

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -18,7 +18,7 @@ session = requests.Session(
 # 转换数据
 basic = [
 	item for item in session.get("https://zip.cm.edu.kg/all.json").json().get('data', [])
-	if "asOrganization" in item["meta"] and item["meta"]["country"] in ["HK", "SG", "TW", "JP", "KR", "US", "GB", "FR", "RU", "SE"]
+	if "asOrganization" in item["meta"] and item["meta"]["country"] in ["HK", "SG", "TW", "JP", "KR", "US", "GB", "FR", "RU", "FI"]
 ]
 
 # 多线程处理
